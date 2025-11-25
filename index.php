96597ec6db24aa9b535e00c7cbbeb2fa9d35ceae
André <111359909+A-thepuma@users.noreply.github.com>
2025-11-25 11:17:52 +0100

    Contraignez l'usage de vos classes

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ class Lobby
 
     public function addPlayer(Player $player)
     {
-        $this->queuingPlayers[] = new QueuingPlayer($player);
+        $this->queuingPlayers[] = new QueuingPlayer($player->getName(), $player->getRatio());
     }
 
     public function addPlayers(Player ...$players)
@@ -41,59 +41,111 @@ class Lobby
     }
 }
 
-class Player
+abstract class Player extends User
 {
-    protected $name;
-    protected $ratio;
-    public function __construct( $name, $ratio = 400.0)
+    protected string $name;
+    protected float $ratio;
+
+    public function __construct(string $name, float $ratio = 400.0)
     {
         $this->name = $name;
         $this->ratio = $ratio;
     }
 
-    public function getName()
+    public function getName(): string
     {
         return $this->name;
     }
 
-    private function probabilityAgainst(self $player)
+    private function probabilityAgainst(self $player): float
     {
         return 1 / (1 + (10 ** (($player->getRatio() - $this->getRatio()) / 400)));
     }
 
-    public function updateRatioAgainst(self $player, int $result)
+    public function updateRatioAgainst(self $player, int $result): void
     {
         $this->ratio += 32 * ($result - $this->probabilityAgainst($player));
     }
 
-    public function getRatio()
+    public function getRatio(): float
     {
         return $this->ratio;
     }
+
 }
 
 class QueuingPlayer extends Player
 {
-    private $range = 1;
+    private int $range = 1;
 
-    public function __construct(Player $player)
+    public function __construct(string $name, float $ratio = 400.0)
     {
-        parent::__construct($player->getName(), $player->getRatio());
+        parent::__construct($name, $ratio);
     }
 
-    public function getRange()
+    public function getRange(): int
     {
         return $this->range;
     }
 
-    public function setRange(int $range)
+    public function setRange(int $range): void
     {
         $this->range = $range;
     }
+
+    public function getUsername(): string
+    {
+        return $this->getName();
+    }
 }
 
-$greg = new Player('greg', 400);
-$jade = new Player('jade', 476);
+
+abstract class User {
+    public const STATUS_ACTIVE = 'active'; public const STATUS_INACTIVE = 'inactive'; 
+    public function __construct(public string $email, public string $status = self::STATUS_ACTIVE) {
+
+     } 
+     public function setStatus(string $status): void { 
+        assert( 
+            in_array($status, [self::STATUS_ACTIVE, self::STATUS_INACTIVE]), 
+            sprintf( 
+                'Le status %s n\'est pas valide. Les status possibles sont : %s', 
+                $status, [self::STATUS_ACTIVE, self::STATUS_INACTIVE]) 
+            ); 
+        $this->status = $status; 
+    } 
+    public function getStatus(): string 
+    { 
+        return $this->status;
+    }
+
+    abstract public function getUsername():string;
+
+}
+
+final class Admin extends User
+{
+    public array $roles;
+
+    public function __construct(string $email, string $status = self::STATUS_ACTIVE, array $roles = [])
+    {
+        parent::__construct($email, $status);
+        $this->roles = $roles;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->email; 
+    }
+}
+
+
+$admin = new Admin('trompete@guy.com', 'Ibrahim Maalouf');
+var_dump($admin);
+
+$greg = new QueuingPlayer('greg', 400);
+$jade = new QueuingPlayer('jade', 476);
+
 
 $lobby = new Lobby();
 $lobby->addPlayers($greg, $jade);
